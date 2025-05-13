da199fa40c2549190ae1a8b8bd76996592436a1a
Michael Shaughnessy <mickeyshaughnessy>
2025-05-12 19:15:44 -0600

    working ping crawler

--- crawler.py
+++ crawler.py
@@ -1,8 +1,19 @@
 # This script crawls the various US federal agency web endpoints.
+import requests
 
-from agency_config import api_urls
+urls = []
+with open("all_urls.dat") as fin:
+    for line in fin:
+        line = line.rstrip()
+        urls.append(line)
 
-for url in api_urls:
-    endpoint = url + '/ping'
-    resp = requests.get(endpoint)
-    print(resp)
+for url in urls:
+    if url[-1] == "/":
+        endpoint = url + 'ping'
+    else:
+        endpoint = url + '/ping'
+    try:
+        resp = requests.get(endpoint)
+        print(resp, endpoint)
+    except:
+        pass
